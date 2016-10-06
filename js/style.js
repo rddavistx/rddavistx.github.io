4862e0930e19aa0ed98e397df7632c6f4cbf37ca
Richard Davis <rddavistx@hotmail.com>
2016-10-05 22:30:55 -0500

    add files

---
+++ js/style.js
@@ -0,0 +1,14 @@
+
+$('#contactMe').click(function() {
+  $('.contact').toggle('slow');
+  return false;
+});
+
+
+
+$('#resumeLink').click(function() {
+  $('.pic').toggle('slow');
+  $('#resume').toggle('slow');
+  return true;
+
+})
